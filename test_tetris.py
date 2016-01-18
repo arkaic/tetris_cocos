56af70c064aee1261b83fc4608106089e77d8a23
Henry Lin <arkhenry@gmail.com>
2016-01-18 01:22:02 -0500

    test: Added comments to roadmap what to do for a test function

--- test_tetris.py
+++ test_tetris.py
@@ -8,6 +8,11 @@ class TestTetrisBoard(unittest.TestCase):
         pass
 
     def test_collapse(self):
+        # After a drop and before the next recursive clear (if there exists new
+        # lines), assert no more clumps can move
+        # If there are new lines, repeat.
+        # Else, check that there are no empty rows
+        # I could also identify all clumps in the board and make sure they can't move
         pass
 
 
