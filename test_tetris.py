5533f5de9de074b0e02bbc318e61b47a219f4169
Henry Lin <arkhenry@gmail.com>
2016-01-17 22:37:52 -0500

    test: Added unittest skeleton for tetris

---
+++ test_tetris.py
@@ -0,0 +1,15 @@
+import unittest
+
+from tetris_game import *
+
+class TestTetrisBoard(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_collapse(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
